6cc239ee26044981c120de059364f70429922330
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-06-04 12:30:43 +0530

    First program using class

---
+++ practice/Practice.py
@@ -0,0 +1,9 @@
+class Practice:
+    def __init__(self, ones, twos, threes):
+        self.one = ones
+        self.two = twos
+        self.three = threes
+p = Practice(1, 2, 3)
+print(p.one)
+print(p.one, p.two)
+print(p.one, p.two, p.three)
