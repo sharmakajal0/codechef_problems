6a9d661e3c7ec79a79fdc93a9c554c8c7778e621
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-06-05 19:36:13 +0530

    Prime Generator

--- EASY/PRIME1.sol.py
+++ EASY/PRIME1.sol.py
@@ -2,11 +2,10 @@
 t = int(input())
 for _ in range(t):
     m, n = map(int, input().split())
-    for num in range(m, n+1):
-        if num>1:
-            for i in range(2, num):
-                if (num % i) == 0:
-                    break
-                
-            else:
-                    print(num)
+for num in range(m, n+1):
+    if num>1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
