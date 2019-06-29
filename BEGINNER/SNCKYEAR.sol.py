23f326d144f9cbfb0ed6533427938832d36ce3ed
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-06-29 09:37:15 +0530

    Chef and Snackdown

---
+++ BEGINNER/SNCKYEAR.sol.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+##
+# Question URL: https://www.codechef.com/problems/SNCKYEAR
+##
+
+for _ in range(int(input())):
+    N = int(input())
+    if N == 2010 or N == 2015 or N == 2016 or N == 2017 or N == 2019:
+        print("HOSTED")
+    else:
+        print("NOT HOSTED")
