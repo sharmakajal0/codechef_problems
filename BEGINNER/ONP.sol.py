6eee4e0202f1c2bbbda5f17ad1901d18ac50742f
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-07-22 15:59:34 +0530

    Transform the Expression

--- BEGINNER/ONP.sol.py
+++ BEGINNER/ONP.sol.py
@@ -1,55 +1,20 @@
-class Conversion:
-    def __init__(self, capacity):
-        self.top = -1
-        self.capacity = capacity
-        self.array = []
-        self.output = []
-        self.precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
-    def isEmpty(self):
-        return True if self.top == -1 else False
-    def peek(self):
-        return self.array[-1]
-    def pop(self):
-        if not self.isEmpty():
-            self.top = -1
-            return self.array.pop()
-        else:
-            return "$"
-    def push(self, op):
-        self.top += 1
-        self.array.append(op)
+T = int(input())
+for _ in range(0, T):
+    expr = list(input())
+    stack = []
+    answer = ''
 
-    def isOperand(self, ch):
-        return ch.isalpha()
-    def notGreater(self, i):
-        try:
-            a = self.precedence[i]
-            b = self.precedence[self.peek()]
-            return True if a <=b else False
-        except KeyError:
-            return False
-    def infixToPostfix(self, exp):
-        for i in exp:
-            if self.isOperand(i):
-                self.output.append(i)
-            elif i == '(':
-                self.push(i)
-            elif i ==')':
-                while((not self.isEmpty()) and self.peek() != '('):
-                    a = self.pop()
-                    self.output.append(a)
-                if (not self.isEmpty() and self.peek() != '('):
-                    return -1
-                else:
-                    self.pop()
-            else:
-                while(not self.isEmpty() and  self.notGreater(i)):
-                    self.output.append(self.pop())
-                self.push(i)
-        while not self.isEmpty():
-            self.output.append(self.pop())
-        print("".join(self.output))
-for _ in range(int(input())):
-    exp = input()
-    obj = Conversion(len(exp))
-    print(obj.infixToPostfix(exp))
+    for i in expr:
+        if (i == '('):
+            stack.append('(')
+        elif (i >= 'a' and i <= 'z'):
+            answer = answer + i
+        elif (i == ')'):
+            while (stack[-1] != '('):
+                answer = answer + stack.pop()
+            
+            stack.pop()
+            print(stack)
+        else:
+            stack.append(i)
+    print(answer)
