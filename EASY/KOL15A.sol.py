653bb83562446f15f3b4d06c1b42ce1b48ea8b1d
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-07-23 15:38:52 +0530

    Processing a string

--- EASY/KOL15A.sol.py
+++ EASY/KOL15A.sol.py
@@ -2,15 +2,17 @@
 
 ''' module for sum of digits in an alphanumeric string'''
 
-def Sum_Of_Digits(S):
+def sum_digits(s_array):
+
     '''Function Definition for calculating the sum of digits in an alphanumeric string'''
     total_sum = 0
-    for x in S:
-        if x.isdigit() == True:
-            z = int(x)
-            total_sum = total_sum + z
+    for i in s_array:
+        if i.isdigit() is True:
+            z_num = int(i)
+            total_sum = total_sum + z_num
     return total_sum
+
 TEST_CASES = int(input())
 for _ in range(TEST_CASES):
     s = input()
-    print(Sum_Of_Digits(s))
+    print(sum_digits(s))
