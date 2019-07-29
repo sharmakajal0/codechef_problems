a3a1a5fbe00c2b195cd03c21634c5fa8812e3b46
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-07-29 21:24:46 +0530

    Chang and the perfect function

---
+++ EASY/CHNGFUNC.sol.py
@@ -0,0 +1,18 @@
+import math
+
+def perfect_function(num_one, num_two):
+
+  '''Function Definition for perfect function'''
+  total = 0
+  for i in range(1, num_one + 1):
+    x = i * i
+    for j in range(1, num_two + 1):
+      f_n = x + j
+      if math.sqrt(f_n) % 1 == 0:
+        total += 1
+      else: total += 0
+
+  print(total)
+
+NUM_ONE, NUM_TWO = map(int, input().split())
+perfect_function(NUM_ONE, NUM_TWO)
