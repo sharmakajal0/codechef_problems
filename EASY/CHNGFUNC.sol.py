5e495e454f031d7009cf2898de6699499c4698cf
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-07-29 23:01:02 +0530

    Chang and the Perfect Function

--- EASY/CHNGFUNC.sol.py
+++ EASY/CHNGFUNC.sol.py
@@ -1,18 +1,20 @@
 import math
-
 def perfect_function(num_one, num_two):
 
   '''Function Definition for perfect function'''
+    
   total = 0
-  for i in range(1, num_one + 1):
-    x = i * i
-    for j in range(1, num_two + 1):
-      f_n = x + j
-      if math.sqrt(f_n) % 1 == 0:
-        total += 1
-      else: total += 0
+  try:
+    for i in range(1, num_one + 1):
+      x = i * i
+      for j in range(1, num_two + 1):
+        f_n = x + j
+        if math.sqrt(f_n) % 1 == 0:
+          total += 1
+        else: total += 0
 
-  print(total)
-
+    print(total)
+  except EOFError:
+    raise EOFError("End of file")
 NUM_ONE, NUM_TWO = map(int, input().split())
 perfect_function(NUM_ONE, NUM_TWO)
